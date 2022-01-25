71fac67b472db7865182b5470c428726a071a36b
Rafael Risalte <risaltte@gmail.com>
2022-01-25 13:47:54 -0400

    Segundo aula

---
+++ pages/chat.js
@@ -0,0 +1,5 @@
+export default function paginaDoChat() {
+    return (
+        <div>Chat</div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from "../config.json";
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style: none;
-            }
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-            /* App fit Height */ 
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-            #__next {
-                flex: 1;
-            }
-            #__next > * {
-                flex: 1;
-            }
-            /* ./App fit Height */ 
-    `}</style>
-    );
-}
+import React from "react";
+import { useRouter } from "next/router";
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -38,7 +11,7 @@ function Titulo(props) {
             <Tag>{props.children}</Tag>
             <style jsx>{`
                 ${Tag} {
-                    color: ${appConfig.theme.colors.neutrals['900']};
+                    color: ${appConfig.theme.colors.neutrals['100']};
                     font-size: 24px;
                     font-weight: 600
                 }
@@ -47,24 +20,12 @@ function Titulo(props) {
     );
 }
 
-// function homePage() {
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Title tag="h2">Boas vindas de volta!</Title>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     );
-// }
-
-// export default homePage;
-
 export default function PaginaInicial() {
-    const username = 'peas';
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +52,10 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={(event) => {
+                            event.preventDefault();
+                            roteamento.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -103,6 +68,8 @@ export default function PaginaInicial() {
 
                         <TextField
                             fullWidth
+                            value={username}
+                            onChange={event => setUsername(event.target.value)}
                             textFieldColors={{
                                 neutral: {
                                     textColor: appConfig.theme.colors.neutrals[200],
@@ -115,6 +82,7 @@ export default function PaginaInicial() {
                         <Button
                             type='submit'
                             label='Entrar'
+                            disabled={ username.length <= 2 }
                             fullWidth
                             buttonColors={{
                                 contrastColor: appConfig.theme.colors.neutrals["000"],
@@ -148,7 +116,7 @@ export default function PaginaInicial() {
                                 borderRadius: '50%',
                                 marginBottom: '16px',
                             }}
-                            src={`https://github.com/${username}.png`}
+                            src={username.length >= 2 ? `https://github.com/${username}.png` : ''}
                         />
                         <Text
                             variant="body4"
